92540734e7723669110a517901e1021ba507938d
PashaZH <79101535010@yandex.ru>
2020-11-22 16:38:58 +0300

    Changes

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,13 +1,13 @@
 @section('header')
 
-<div class="d-flex  flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <h5 class="my-0 mr-md-auto font-weight-normal">Cook Teacher</h5>
+<div class="d-flex  flex-column flex-md-row align-items-center p-3 px-md-4 mb-3   top-block border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">Cooking teacher</h5>
   <nav class="my-2 my-md-0 mr-md-3">
     <a class="p-2 text-dark" href="{{route('homepage')}}">Главная</a>
     <a class="p-2 text-dark" href="{{route('contact')}}">Контакты</a>
     <a class="p-2 text-dark" href="{{route('about')}}">О нас</a>
   </nav>
-  <a class="btn btn-outline-primary" href="#">Sign up</a>
+  <a class="btn btn-outline-primary" href="#">Войти</a>
 </div>
 
  
--- resources/views/inc/topslider.blade.php
+++ resources/views/inc/topslider.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-<div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+<div class="jumbotron p-4 p-md-5 text-white rounded bg-dark" style="background-image:url('/img/slide_img.jpg');background-size: cover;">
     <div class="col-md-6 px-0">
       <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
       <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
